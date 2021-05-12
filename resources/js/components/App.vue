f60500da18af8e5c06d3491862dffcffd4c96361
minhtien020900 <minhtien020900@gmail.com>
2021-05-12 17:04:08 +0700

    Add vue in project

---
+++ resources/js/components/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+  App 
+  <br>
+  <ExampleComponent></ExampleComponent>
+  </div>
+</template>
+
+<script>
+import ExampleComponent from './ExampleComponent.vue'
+export default {
+  components: { ExampleComponent },
+
+}
+</script>
+
+<style>
+
+</style>
